b9b812ff7177a556e1d47b7475dfe3d0be430694
TanakitInt <imTanakit@gmail.com>
2019-05-04 23:02:24 +0700

    R code update
    add code into function

--- graph.r
+++ graph.r
@@ -1,15 +1,18 @@
 setwd("D:/TanakitInt/Documents/GitHub/email-analysis")
 
-email = read.csv("output/emailProvider.csv")
+graphPlot = function()
+{
+    email = read.csv("output/emailProvider.csv")
 
-count = table(emailList$email_provider)
+    count = table(email$email_provider)
 
-count_sorted = sort(count, decreasing = TRUE)
+    count_sorted = sort(count, decreasing = TRUE)
 
-color = c('#8570ca', '#dc2453', '#68b122', '#a0a9f5', 
-          '#bb6754', '#148bbb', '#ad106a', '#c8b120', 
-          '#c249ef', '#2d5779', '#2ff1b8', '#c49c3c',
-          '#e37c8e', '#a61a99', '#c647fa', '#8e45f6', 
-          '#d5ce24')
+    color = c('#8570ca', '#dc2453', '#68b122', '#a0a9f5', 
+              '#bb6754', '#148bbb', '#ad106a', '#c8b120', 
+              '#c249ef', '#2d5779', '#2ff1b8', '#c49c3c',
+              '#e37c8e', '#a61a99', '#c647fa', '#8e45f6', 
+              '#d5ce24')
 
-pie(count_sorted, main = "Count of Email users", col = color)
+    pie(count_sorted, main = "Count of Email users", col = color)
+}
